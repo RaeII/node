a799a376a5312f75e7b9a7f45f3e505b69c0d22d
Your Name <78545462+RaeII@users.noreply.github.com>
2022-06-07 21:03:35 -0300

    http final

--- https/server.js
+++ https/server.js
@@ -1,6 +1,6 @@
 //servidor https
 //npm i para recarregar os modulos
-//nodemon packge.json vai monitorar toda mudança no arquivo e vai atualizar sem precisar reiniciar
+//nodemon packge.json vai monitorar toda mudança no arquivo e vai atualizar sem precisar reiniciar(trocar start no json)
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
@@ -8,16 +8,22 @@ const path = require('path')
 
 //req - pedido
 //red - resposta
+//todos os arquivos são recebidos por uma requisição e passando pelo file
 http.createServer((req, res)=>{
     const file = req.url === '/' ? 'index.html' : req.url
-    console.log(file)
-    res.end('chegou')
+    const filePath = path.join(__dirname,'cuonline',file)
+    const extName = path.extname(filePath)
+    
+    const allowedFileTypes = ['.html','.css','.js','.png','.jpg','.jpeg','.svg']
+    const allowed = allowedFileTypes.find(item => item == extName)
 
-    // if(req.url == '/'){
-    //    fs.readFile(path.join(__dirname,'cuonline','index.html'),(err,content)=>{
-    //                             if(err) throw err
-    //                             res.end(content)
-    //                         })
-    // }
+    if(!allowed) return
+
+       console.log(req.url) 
+       fs.readFile(filePath,(err,content)=>{
+                                if(err) throw err
+                                res.end(content) 
+                            })
+   
 
 }).listen(3000, () => console.log('server is running'))
