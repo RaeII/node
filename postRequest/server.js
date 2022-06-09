5e574a2908b7e419e1e943efcbab24d63bf54e1d
Your Name <78545462+RaeII@users.noreply.github.com>
2022-06-09 20:25:16 -0300

    final https and post

---
+++ postRequest/server.js
@@ -0,0 +1,29 @@
+//servidor https
+//npm i para recarregar os modulos
+//nodemon packge.json vai monitorar toda mudança no arquivo e vai atualizar sem precisar reiniciar(trocar start no json)
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+
+//req - pedido
+//res - resposta
+//todos os arquivos são recebidos por uma requisição e passando pelo file
+http.createServer((req, res)=>{
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname,'urls',file)
+    const extName = path.extname(filePath)
+    
+    const allowedFileTypes = ['.html','.css','.js','.png','.jpg','.jpeg','.svg']
+    const allowed = allowedFileTypes.find(item => item == extName)
+
+    if(!allowed) return
+
+       console.log(req.url) 
+       fs.readFile(filePath,(err,content)=>{
+                                if(err) throw err
+                                res.end(content) 
+                            })
+   
+
+}).listen(3000, () => console.log('server is running'))
